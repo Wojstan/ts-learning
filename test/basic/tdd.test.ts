f06ecc6c2573a3b2e03de0f49588370fd4b923cf
Wojstan <wpstaniszewski@gmail.com>
2023-09-09 16:38:18 +0200

    🔞 Remove only from tests

--- test/basic/tdd.test.ts
+++ test/basic/tdd.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { add } from '../../src/basic/tdd'
 
-describe.only('tdd', () => {
+describe('tdd', () => {
   it('add', () => {
     expect(add(2, 3)).to.equal(5)
   })
